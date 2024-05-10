75fc42bac0d3e969bc9e3d422ddbf9ec186000e8
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-05-10 19:28:31 +0700

    (Setup Project) : Frontend HydroSense

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Tes</div>,
+  },
+  {
+    path: "/home",
+    element: <HomePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="bg-black">
+      <h1 className="text-cyan-400">Tes Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
